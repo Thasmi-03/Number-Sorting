f7f53d6e22614716fc0c66c07d5677fb3f422e67
Thasmiga <vimalthasmiga1112@gmail.com>
2025-09-04 14:59:29 +0530

    number sorting

---
+++ script.js
@@ -0,0 +1,36 @@
+ 
+        function sort(event) {
+            event.preventDefault();
+            
+            const num1 = parseFloat(document.getElementById("num1").value);
+            const num2 = parseFloat(document.getElementById("num2").value);
+            const num3 = parseFloat(document.getElementById("num3").value);
+            const order = document.getElementById("sortOrder").value;
+
+
+
+            let number = [num1, num2, num3];
+            if (order === "asc") {
+                number.sort((x, y) => x - y);
+            }
+            else if (order === "des") {
+                number.sort((x, y) => y - x);
+
+            } else {
+                return;
+            }
+
+
+
+
+
+            document.getElementById("resultNumber").innerHTML = `Sorted Numbers: ${number}`;
+        }
+
+
+
+        function clearFields() {
+            document.getElementById('num1').value = '';
+            document.getElementById('num2').value = '';
+            document.getElementById('num3').value = '';
+        } document.getElementById('resultNumber').innerHTML = " ";
